b23f5d6b35f45cc21704a54be5bdf074f6869cfb
nayon117 <hasibul.nayon1@gmail.com>
2024-09-05 21:50:31 +0600

    create profile component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import UserProfile from "./components/UserProfile";
+
+const App = () => {
+  return (
+    <div className='bg-[#212121]'>
+       <UserProfile />
+    </div>
+  )
+}
+export default App;
